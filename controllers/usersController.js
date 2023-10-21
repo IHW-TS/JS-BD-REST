2435278f8e23944d30f6dc8885c4343779c3d240
TmnSyk <78849961+IHW-TS@users.noreply.github.com>
2023-10-21 13:14:58 +0200

    vf

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,43 +5,66 @@ const data = {
   },
 };
 
-const getAllUsers = () => {};
+const getAllUsers = () => {
+  return data.users;
+};
 
 const createNewUser = (body) => {
   // create newUser
+  const newUser = {
+    id: data.users.length + 1,
+    firstname: body.firstname,
+    lastname: body.lastname
+  };
 
   //update data users created new user
+  data.users.push(newUser);
 
   return data.users;
 };
 
 const updateUser = (body) => {
   // data users find user
+  const user = data.users.find(user => user.id === body.id);
 
   // update user
+  user.firstname = body.firstname;
+  user.lastname = body.lastname;
 
   // filter data users !user
+  const updatedUsers = data.users.filter(user => user.id !== body.id);
 
   //update data users with updated user
+  data.users = [...updatedUsers, user];
 
   return data.users;
 };
 
 const deleteUser = (body) => {
   // data users find user
+  const user = data.users.find(user => user.id === body.id);
 
   // filter data users !user
+  const updatedUsers = data.users.filter(user => user.id !== body.id);
 
   //update data users
+  data.users = updatedUsers;
 
   return data.users;
 };
 
 const getUser = (body) => {
   // data users find user
+  const user = data.users.find(user => user.id === body.id);
 
   return user;
 };
+
+console.log(getAllUsers());
+console.log(createNewUser({ firstname: 'D', lastname: 'D' }));
+console.log(updateUser({ id: 3, firstname: 'Dupont', lastname: 'D' }));
+console.log(deleteUser({ id: 3 }));
+console.log(getUser({ id: 1 }));
 
 module.exports = {
   getAllUsers,
